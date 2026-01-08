427176bdb4ca2c99b977b5a09aa0fd99c69a0798
Rafael Nuansa <iandrewnew@gmail.com>
2026-01-08 18:37:50 +0700

    changes: title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ $title ?? 'Dashboard' }} | Rafael Dashboard</title>
+    <title>{{ $title ?? 'Dashboard' }} | Laravael UI</title>
 
     {{-- Fonts & Icons --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
